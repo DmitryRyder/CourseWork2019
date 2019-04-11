4c0d74f261cf30f41333fbb394f71c5b67ed0b17
ryderzxz <dmitryryder91@gmail.com>
2019-04-12 02:53:21 +0300

    в

---
+++ API.Core/Automapper/Profiles/OrganizationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Common.DTO;
+using Common.Models;
+
+namespace API.Core.Automapper.Profiles
+{
+    class OrganizationProfile : Profile
+    {
+        public OrganizationProfile()
+        {
+            CreateMap<Organization, OrganizationDto>();
+            CreateMap<OrganizationDto, Organization>();
+        }
+    }
+}
